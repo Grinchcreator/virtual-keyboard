c70f559d0fd8765d8b200e9305a796009cc259c5
Grinchcreator <119819551+Grinchcreator@users.noreply.github.com>
2023-05-03 14:53:44 +0300

    fix: js fixed (language added and caps/shift)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("This keyboard is made on Windows OS");
+alert("This keyboard is made on Windows OS. To change language press CTRL+ALT+CAPS");
 
 //create text area and classes
 let textarea = document.createElement("TEXTAREA");
@@ -311,8 +311,8 @@ let btn_alt = document.createElement("button");
   keyboard__keys5.appendChild(btn_alt);
 
 let btn_space = document.createElement("button");
-  btn_space.className = "keyboard__key__2";
-  btn_space.innerHTML = "";
+  btn_space.className = "keyboard__key__3";
+  btn_space.innerHTML = " ";
   keyboard__keys5.appendChild(btn_space);
 
 let btn_alt2 = document.createElement("button");
@@ -328,11 +328,11 @@ let btn_ctrl2 = document.createElement("button");
 document.body.append(keyboard);
 
 
-// clear textarea by clicking
-textarea.onclick = function clear () {
+// clear textarea by clicking del
+btn_del.onclick = function clear () {
   textarea.value = "";
 }
-
+//text typing
 const buttons = document.querySelectorAll(".keyboard__key");
   buttons.forEach(btn=>{
     btn.addEventListener('click', () => {
@@ -340,8 +340,46 @@ const buttons = document.querySelectorAll(".keyboard__key");
     })
 });
 
+btn_enter.addEventListener('click', () => {
+    textarea.value += '\r\n';
+})
+  
+btn_space.addEventListener('click', () => {
+    textarea.value += " ";
+})
+  
+btn_backspace.addEventListener('click', () => {
+    let string = textarea.value;
+    str = string.slice(0, -1)
+    textarea.value = str;
+})
+let altClicked = false;
+let ctrlClicked = false;
 
-btn_caps.onclick = function capitalize () {
+btn_alt.addEventListener('click', () => {
+  altClicked = true;
+  if(altClicked && ctrlClicked) languageRUS();
+});
+  
+btn_ctrl.addEventListener('click', () => {
+  ctrlClicked = true;
+  if (altClicked && ctrlClicked) languageRUS ();
+})
+
+btn_alt2.addEventListener('click', () => {
+    altClicked = true;
+    if(altClicked && ctrlClicked) languageRUS();
+});
+    
+  btn_ctrl2.addEventListener('click', () => {
+    ctrlClicked = true;
+    if (altClicked && ctrlClicked) languageRUS ();
+});
+
+let caps = 0;
+btn_caps.addEventListener('click', () => {
+
+  if (caps==0) {
     btn_q.innerText = "Q"; btn_w.innerText = "W"; btn_e.innerText = "E";
     btn_r.innerText = "R"; btn_t.innerText = "T"; btn_y.innerText = "Y"; 
     btn_u.innerText = "U"; btn_i.innerText = "I"; btn_o.innerText = "O"; 
@@ -351,5 +389,275 @@ btn_caps.onclick = function capitalize () {
     btn_l.innerText = "L"; btn_z.innerText = "Z"; btn_x.innerText = "X"; 
     btn_c.innerText = "C"; btn_v.innerText = "V"; btn_b.innerText = "B";
     btn_n.innerText = "N"; btn_m.innerText = "M";
+    caps = 1;
+  } 
+  else 
+  {
+    btn_q.innerText = "q"; btn_w.innerText = "w"; btn_e.innerText = "e";
+    btn_r.innerText = "r"; btn_t.innerText = "t"; btn_y.innerText = "y"; 
+    btn_u.innerText = "u"; btn_i.innerText = "i"; btn_o.innerText = "o"; 
+    btn_p.innerText = "p"; btn_a.innerText = "a"; btn_s.innerText = "s"; 
+    btn_d.innerText = "d"; btn_f.innerText = "f"; btn_g.innerText = "g"; 
+    btn_h.innerText = "h"; btn_j.innerText = "j"; btn_k.innerText = "k"; 
+    btn_l.innerText = "l"; btn_z.innerText = "z"; btn_x.innerText = "x"; 
+    btn_c.innerText = "c"; btn_v.innerText = "v"; btn_b.innerText = "b";
+    btn_n.innerText = "n"; btn_m.innerText = "m";
+    caps = 0;
+  };
+});
+
+let shift = 0;
+btn_shift.addEventListener('click', () => {
+  if (shift == 0) {
+    btn_quotes_angled.innerText = "~"; btn_1.innerText = "!"; btn_2.innerText = "@";
+    btn_3.innerText = "#"; btn_4.innerText = "$"; btn_5.innerText = "%"; 
+    btn_6.innerText = "^"; btn_7.innerText = "&"; btn_8.innerText = "*"; 
+    btn_9.innerText = "("; btn_0.innerText = ")"; btn_minus.innerText = "_"; btn_equal.innerText = "+";
+    btn_bracket_left.innerText = "{"; btn_bracket_right.innerText = "}"; btn_semicolumn.innerText = ":";
+    btn_quotes.innerText = '"'; btn_comma.innerText = "<"; btn_period.innerText = ">"; btn_slash.innerText = "?";
+    shift = 1;
+  }
+  else
+  {
+    btn_quotes_angled.innerText = "`"; btn_1.innerText = "1"; btn_2.innerText = "2";
+    btn_3.innerText = "3"; btn_4.innerText = "4"; btn_5.innerText = "5"; 
+    btn_6.innerText = "6"; btn_7.innerText = "7"; btn_8.innerText = "8"; 
+    btn_9.innerText = "9"; btn_0.innerText = "0"; btn_minus.innerText = "-"; btn_equal.innerText = "=";
+    btn_bracket_left.innerText = "["; btn_bracket_right.innerText = "]"; btn_semicolumn.innerText = ";";
+    btn_quotes.innerText = "'"; btn_comma.innerText = ","; btn_period.innerText = "."; btn_slash.innerText = "/";
+    shift = 0; 
+  };
+});
+
+btn_shift2.addEventListener('click', () => {
+  if (shift == 0) {
+    btn_quotes_angled.innerText = "~"; btn_1.innerText = "!"; btn_2.innerText = "@";
+    btn_3.innerText = "#"; btn_4.innerText = "$"; btn_5.innerText = "%"; 
+    btn_6.innerText = "^"; btn_7.innerText = "&"; btn_8.innerText = "*"; 
+    btn_9.innerText = "("; btn_0.innerText = ")"; btn_minus.innerText = "_"; btn_equal.innerText = "+";
+    btn_bracket_left.innerText = "{"; btn_bracket_right.innerText = "}"; btn_semicolumn.innerText = ":";
+    btn_quotes.innerText = '"'; btn_comma.innerText = "<"; btn_period.innerText = ">"; btn_slash.innerText = "?";
+    shift = 1;
+  }
+  else
+  {
+    btn_quotes_angled.innerText = "`"; btn_1.innerText = "1"; btn_2.innerText = "2";
+    btn_3.innerText = "3"; btn_4.innerText = "4"; btn_5.innerText = "5"; 
+    btn_6.innerText = "6"; btn_7.innerText = "7"; btn_8.innerText = "8"; 
+    btn_9.innerText = "9"; btn_0.innerText = "0"; btn_minus.innerText = "-"; btn_equal.innerText = "=";
+    btn_bracket_left.innerText = "["; btn_bracket_right.innerText = "]"; btn_semicolumn.innerText = ";";
+    btn_quotes.innerText = "'"; btn_comma.innerText = ","; btn_period.innerText = "."; btn_slash.innerText = "/";
+    shift = 0; 
+  };
+});
+
+function languageENG(){
+let capsENG = 0;
+btn_caps.addEventListener('click', () => {
+
+  if (capsENG==0) {
+    btn_q.innerText = "Q"; btn_w.innerText = "W"; btn_e.innerText = "E";
+    btn_r.innerText = "R"; btn_t.innerText = "T"; btn_y.innerText = "Y"; 
+    btn_u.innerText = "U"; btn_i.innerText = "I"; btn_o.innerText = "O"; 
+    btn_p.innerText = "P"; btn_a.innerText = "A"; btn_s.innerText = "S"; 
+    btn_d.innerText = "D"; btn_f.innerText = "F"; btn_g.innerText = "G"; 
+    btn_h.innerText = "H"; btn_j.innerText = "J"; btn_k.innerText = "K"; 
+    btn_l.innerText = "L"; btn_z.innerText = "Z"; btn_x.innerText = "X"; 
+    btn_c.innerText = "C"; btn_v.innerText = "V"; btn_b.innerText = "B";
+    btn_n.innerText = "N"; btn_m.innerText = "M";
+    capsENG = 1;
+  } 
+  else 
+  {
+    btn_q.innerText = "q"; btn_w.innerText = "w"; btn_e.innerText = "e";
+    btn_r.innerText = "r"; btn_t.innerText = "t"; btn_y.innerText = "y"; 
+    btn_u.innerText = "u"; btn_i.innerText = "i"; btn_o.innerText = "o"; 
+    btn_p.innerText = "p"; btn_a.innerText = "a"; btn_s.innerText = "s"; 
+    btn_d.innerText = "d"; btn_f.innerText = "f"; btn_g.innerText = "g"; 
+    btn_h.innerText = "h"; btn_j.innerText = "j"; btn_k.innerText = "k"; 
+    btn_l.innerText = "l"; btn_z.innerText = "z"; btn_x.innerText = "x"; 
+    btn_c.innerText = "c"; btn_v.innerText = "v"; btn_b.innerText = "b";
+    btn_n.innerText = "n"; btn_m.innerText = "m";
+    capsENG = 0;
+  };
+});
+
+let shiftENG = 0;
+btn_shift.addEventListener('click', () => {
+  if (shiftENG == 0) {
+    btn_quotes_angled.innerText = "~"; btn_1.innerText = "!"; btn_2.innerText = "@";
+    btn_3.innerText = "#"; btn_4.innerText = "$"; btn_5.innerText = "%"; 
+    btn_6.innerText = "^"; btn_7.innerText = "&"; btn_8.innerText = "*"; 
+    btn_9.innerText = "("; btn_0.innerText = ")"; btn_minus.innerText = "_"; btn_equal.innerText = "+";
+    btn_bracket_left.innerText = "{"; btn_bracket_right.innerText = "}"; btn_semicolumn.innerText = ":";
+    btn_quotes.innerText = '"'; btn_comma.innerText = "<"; btn_period.innerText = ">"; btn_slash.innerText = "?";
+    shiftENG = 1;
+  }
+  else
+  {
+    btn_quotes_angled.innerText = "`"; btn_1.innerText = "1"; btn_2.innerText = "2";
+    btn_3.innerText = "3"; btn_4.innerText = "4"; btn_5.innerText = "5"; 
+    btn_6.innerText = "6"; btn_7.innerText = "7"; btn_8.innerText = "8"; 
+    btn_9.innerText = "9"; btn_0.innerText = "0"; btn_minus.innerText = "-"; btn_equal.innerText = "=";
+    btn_bracket_left.innerText = "["; btn_bracket_right.innerText = "]"; btn_semicolumn.innerText = ";";
+    btn_quotes.innerText = "'"; btn_comma.innerText = ","; btn_period.innerText = "."; btn_slash.innerText = "/";
+    shiftENG = 0; 
+  };
+});
+
+btn_shift2.addEventListener('click', () => {
+  if (shiftENG == 0) {
+    btn_quotes_angled.innerText = "~"; btn_1.innerText = "!"; btn_2.innerText = "@";
+    btn_3.innerText = "#"; btn_4.innerText = "$"; btn_5.innerText = "%"; 
+    btn_6.innerText = "^"; btn_7.innerText = "&"; btn_8.innerText = "*"; 
+    btn_9.innerText = "("; btn_0.innerText = ")"; btn_minus.innerText = "_"; btn_equal.innerText = "+";
+    btn_bracket_left.innerText = "{"; btn_bracket_right.innerText = "}"; btn_semicolumn.innerText = ":";
+    btn_quotes.innerText = '"'; btn_comma.innerText = "<"; btn_period.innerText = ">"; btn_slash.innerText = "?";
+    shiftENG = 1;
+  }
+  else
+  {
+    btn_quotes_angled.innerText = "`"; btn_1.innerText = "1"; btn_2.innerText = "2";
+    btn_3.innerText = "3"; btn_4.innerText = "4"; btn_5.innerText = "5"; 
+    btn_6.innerText = "6"; btn_7.innerText = "7"; btn_8.innerText = "8"; 
+    btn_9.innerText = "9"; btn_0.innerText = "0"; btn_minus.innerText = "-"; btn_equal.innerText = "=";
+    btn_bracket_left.innerText = "["; btn_bracket_right.innerText = "]"; btn_semicolumn.innerText = ";";
+    btn_quotes.innerText = "'"; btn_comma.innerText = ","; btn_period.innerText = "."; btn_slash.innerText = "/";
+    shiftENG = 0; 
+  };
+});
+};
+
+function languageRUS(){
+
+  let capsRUS = 0;
+
+  btn_quotes_angled.innerText = "ё";
+  btn_q.innerText = "й"; btn_w.innerText = "ц"; btn_e.innerText = "у";
+  btn_r.innerText = "к"; btn_t.innerText = "е"; btn_y.innerText = "н"; 
+  btn_u.innerText = "г"; btn_i.innerText = "ш"; btn_o.innerText = "щ"; 
+  btn_p.innerText = "з"; btn_bracket_left.innerText = "х"; btn_bracket_right.innerText = "ъ";
+  btn_a.innerText = "ф"; btn_s.innerText = "ы"; 
+  btn_d.innerText = "в"; btn_f.innerText = "а"; btn_g.innerText = "п"; 
+  btn_h.innerText = "р"; btn_j.innerText = "о"; btn_k.innerText = "л"; 
+  btn_l.innerText = "д"; btn_semicolumn.innerText = "ж"; btn_quotes.innerText = "э";
+  btn_z.innerText = "я"; btn_x.innerText = "ч"; 
+  btn_c.innerText = "с"; btn_v.innerText = "м"; btn_b.innerText = "и";
+  btn_n.innerText = "т"; btn_m.innerText = "ь"; btn_comma.innerText = "б";
+  btn_period.innerText = "ю"; btn_slash.innerText = ".";
+
+  btn_caps.addEventListener('click', () => {
+    if (capsRUS==0){
+        btn_quotes_angled.innerText = "Ё";
+        btn_q.innerText = "Й"; btn_w.innerText = "Ц"; btn_e.innerText = "У";
+        btn_r.innerText = "К"; btn_t.innerText = "Е"; btn_y.innerText = "Н"; 
+        btn_u.innerText = "Г"; btn_i.innerText = "Ш"; btn_o.innerText = "Щ"; 
+        btn_p.innerText = "З"; btn_bracket_left.innerText = "Х"; btn_bracket_right.innerText = "Ъ";
+        btn_a.innerText = "Ф"; btn_s.innerText = "Ы"; 
+        btn_d.innerText = "В"; btn_f.innerText = "А"; btn_g.innerText = "П"; 
+        btn_h.innerText = "Р"; btn_j.innerText = "О"; btn_k.innerText = "Л"; 
+        btn_l.innerText = "Д"; btn_semicolumn.innerText = "Ж"; btn_quotes.innerText = "Э";
+        btn_z.innerText = "Я"; btn_x.innerText = "Ч"; 
+        btn_c.innerText = "С"; btn_v.innerText = "М"; btn_b.innerText = "И";
+        btn_n.innerText = "Т"; btn_m.innerText = "Ь"; btn_comma.innerText = "Б";
+        btn_period.innerText = "Ю";
+        capsRUS = 1;
+    } else {
+        btn_quotes_angled.innerText = "ё";
+        btn_q.innerText = "й"; btn_w.innerText = "ц"; btn_e.innerText = "у";
+        btn_r.innerText = "к"; btn_t.innerText = "е"; btn_y.innerText = "н"; 
+        btn_u.innerText = "г"; btn_i.innerText = "ш"; btn_o.innerText = "щ"; 
+        btn_p.innerText = "з"; btn_bracket_left.innerText = "х"; btn_bracket_right.innerText = "ъ";
+        btn_a.innerText = "ф"; btn_s.innerText = "ы"; 
+        btn_d.innerText = "в"; btn_f.innerText = "а"; btn_g.innerText = "п"; 
+        btn_h.innerText = "р"; btn_j.innerText = "о"; btn_k.innerText = "л"; 
+        btn_l.innerText = "д"; btn_semicolumn.innerText = "ж"; btn_quotes.innerText = "э";
+        btn_z.innerText = "я"; btn_x.innerText = "ч"; 
+        btn_c.innerText = "с"; btn_v.innerText = "м"; btn_b.innerText = "и";
+        btn_n.innerText = "т"; btn_m.innerText = "ь"; btn_comma.innerText = "б";
+        btn_period.innerText = "ю";
+        capsRUS = 0;
+    }
+  });
+  
+  let shiftRUS = 0;
+  btn_shift.addEventListener('click', () => {
+    if (shift == 0) {
+      btn_quotes_angled.innerText = "Ё";
+      btn_1.innerText = "!"; btn_2.innerText = '"';
+      btn_3.innerText = "№"; btn_4.innerText = ";"; btn_5.innerText = "%"; 
+      btn_6.innerText = ":"; btn_7.innerText = "?"; btn_8.innerText = "*"; 
+      btn_9.innerText = "("; btn_0.innerText = ")"; btn_minus.innerText = "_"; btn_equal.innerText = "+";
+      btn_bracket_left.innerText = "Х"; btn_bracket_right.innerText = "Ъ";
+      btn_semicolumn.innerText = "Ж"; btn_quotes.innerText = "Э";
+      btn_comma.innerText = "Б"; btn_period.innerText = "Ю";
+      btn_slash.innerText = ",";
+      shiftRUS = 1;
+    }
+    else
+    {
+      btn_quotes_angled.innerText = "ё";
+      btn_1.innerText = "1"; btn_2.innerText = "2";
+      btn_3.innerText = "3"; btn_4.innerText = "4"; btn_5.innerText = "5"; 
+      btn_6.innerText = "6"; btn_7.innerText = "7"; btn_8.innerText = "8"; 
+      btn_9.innerText = "9"; btn_0.innerText = "0"; btn_minus.innerText = "-"; btn_equal.innerText = "=";
+      btn_bracket_left.innerText = "х"; btn_bracket_right.innerText = "ъ";
+      btn_semicolumn.innerText = "ж"; btn_quotes.innerText = "э";
+      btn_comma.innerText = "б"; btn_period.innerText = "ю";
+      btn_slash.innerText = ".";
+      shiftRUS = 0; 
+    };
+  });
+  
+  btn_shift2.addEventListener('click', () => {
+    if (shiftRUS == 0) {
+        btn_quotes_angled.innerText = "Ё";
+        btn_1.innerText = "!"; btn_2.innerText = '"';
+        btn_3.innerText = "№"; btn_4.innerText = ";"; btn_5.innerText = "%"; 
+        btn_6.innerText = ":"; btn_7.innerText = "?"; btn_8.innerText = "*"; 
+        btn_9.innerText = "("; btn_0.innerText = ")"; btn_minus.innerText = "_"; btn_equal.innerText = "+";
+        btn_bracket_left.innerText = "Х"; btn_bracket_right.innerText = "Ъ";
+        btn_semicolumn.innerText = "Ж"; btn_quotes.innerText = "Э";
+        btn_comma.innerText = "Б"; btn_period.innerText = "Ю";
+        btn_slash.innerText = ",";
+        shiftRUS = 1;
+      }
+      else
+      {
+        btn_quotes_angled.innerText = "ё";
+        btn_1.innerText = "1"; btn_2.innerText = "2";
+        btn_3.innerText = "3"; btn_4.innerText = "4"; btn_5.innerText = "5"; 
+        btn_6.innerText = "6"; btn_7.innerText = "7"; btn_8.innerText = "8"; 
+        btn_9.innerText = "9"; btn_0.innerText = "0"; btn_minus.innerText = "-"; btn_equal.innerText = "=";
+        btn_bracket_left.innerText = "х"; btn_bracket_right.innerText = "ъ";
+        btn_semicolumn.innerText = "ж"; btn_quotes.innerText = "э";
+        btn_comma.innerText = "б"; btn_period.innerText = "ю";
+        btn_slash.innerText = ".";
+        shiftRUS = 0; 
+      };
+  });
+
+  let altClickedRUS= false; 
+  let ctrlClickedRUS = false;
+
+  btn_alt.addEventListener('click', () => {
+    altClickedRUS= true;
+    if(altClickedRUS && ctrlClickedRUS) languageENG();
+  });
+    
+  btn_ctrl.addEventListener('click', () => {
+    ctrlClickedRUS = true;
+    if (altClickedRUS && ctrlClickedRUS) languageENG ();
+  });
+  
+  btn_alt2.addEventListener('click', () => {
+      altClickedRUS= true;
+      if(altClickedRUS && ctrlClickedRUS) languageENG();
+  });
+      
+    btn_ctrl2.addEventListener('click', () => {
+      ctrlClickedRUS = true;
+      if (altClickedRUS && ctrlClickedRUS) languageENG ();
+  });  
 }
 
